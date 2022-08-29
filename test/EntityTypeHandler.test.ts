aef51aeed9f80600c1605d951d132b7c9bcebabb
Jim Driscoll <jim.a.driscoll@gmail.com>
2022-08-29 16:41:45 +0100

    Test iterated

--- test/EntityTypeHandler.test.ts
+++ test/EntityTypeHandler.test.ts
@@ -17,4 +17,22 @@ describe("Entity Type handling", () => {
         expect(response).to.haveOwnProperty("included").which.is.instanceOf(Array).with.ownProperty("length").eq(1)
         expect(response.included[0].attributes).to.haveOwnProperty("name").eq("D Adams")
     })
+    it("can handle a big JOINed response (iterated)", () => {
+        function *responseIterator(count: number) {
+            for(let i = 0; i < count; i++) {
+                yield {
+                    id: "" + (i + 1),
+                    author: {
+                        id: "42",
+                        name: "D Adams",
+                    },
+                }
+            }
+        }
+        const response = handler.postProcess(responseIterator(10000), 10000, "book")
+        expect(response).to.haveOwnProperty("data").which.is.instanceOf(Array).with.ownProperty("length").eq(10000)
+        expect(response.data[0].relationships.author).to.haveOwnProperty("data").which.deep.eq({type: "author", id: "42"})
+        expect(response).to.haveOwnProperty("included").which.is.instanceOf(Array).with.ownProperty("length").eq(1)
+        expect(response.included[0].attributes).to.haveOwnProperty("name").eq("D Adams")
+    })
 })
